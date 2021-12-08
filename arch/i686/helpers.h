8b863c6dd2a476fb92f83fb4b7e5d268a0454f39
Anton Gusev <uartman@mail.ru>
2021-12-08 17:27:58 +0300

    x86: fix inw, inl helpers

--- arch/i686/helpers.h
+++ arch/i686/helpers.h
@@ -35,7 +35,7 @@ static inline uint8_t inb(uint16_t port)
 static inline uint16_t inw(uint16_t port)
 {
     uint16_t ret;
-    asm volatile ( "inb %1, %0"
+    asm volatile ( "inw %1, %0"
     : "=a"(ret)
     : "Nd"(port) );
     return ret;
@@ -44,7 +44,7 @@ static inline uint16_t inw(uint16_t port)
 static inline uint32_t inl(uint16_t port)
 {
     uint32_t ret;
-    asm volatile ( "inb %1, %0"
+    asm volatile ( "inl %1, %0"
     : "=a"(ret)
     : "Nd"(port) );
     return ret;
