01f3912b5dec0d0be497a05e4db056198258f034
Anton Gusev <uartman@mail.ru>
2021-12-11 18:39:21 +0300

    misc: fix boot on thinkpad

--- third_party/printf/printf.c
+++ third_party/printf/printf.c
@@ -898,7 +898,7 @@ int printf_(const char* format, ...)
 {
     va_list va;
     va_start(va, format);
-    char buffer[100];
+    char buffer[1];
     const int ret = _vsnprintf(_out_char, buffer, (size_t)-1, format, va);
     va_end(va);
     return ret;
