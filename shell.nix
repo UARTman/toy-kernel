d1b86b2044da78ea5c17db708219f2acf955cba9
Anton Gusev <uartman@mail.ru>
2021-12-25 22:37:06 +0300

    misc: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {}, 
+  pkgsCross ? import <nixpkgs> {
+    crossSystem = {
+      config = "i686-elf";
+    };
+  } }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [ 
+      pkgs.meson 
+      pkgs.ninja 
+      pkgs.qemu
+      pkgs.grub2
+      pkgs.xorriso
+      pkgsCross.buildPackages.gcc
+    ];
+    
+}
